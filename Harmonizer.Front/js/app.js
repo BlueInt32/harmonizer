d250a89ee49f8a47b7ed01216e687a5b680d20eb
BlueInt32 <simon.budin@gmail.com>
2014-11-07 00:11:54 +0100

    first commit

---
+++ Harmonizer.Front/js/app.js
@@ -0,0 +1,1 @@
+﻿var harmonizerApp = angular.module('harmonizerApp', []);
---
+++ Harmonizer.Front/js/controllers/mainController.js
@@ -0,0 +1,15 @@
+﻿harmonizerApp.controller('mainController', ['$scope',
+	function($scope)
+	{
+		$scope.notes = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+		$scope.chordTypes = ['Major Triad', 'Minor Triad'];
+
+		$scope.insertChord = function()
+		{
+			alert($scope.noteChosen);
+			alert($scope.chordChosen);
+
+
+		};
+	}
+]);
