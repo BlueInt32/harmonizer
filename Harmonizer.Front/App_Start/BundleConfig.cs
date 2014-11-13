5dd28447c8e2e6f09c1da7daa8b0e284d1b2183e
BlueInt32 <simon.budin@gmail.com>
2014-11-13 08:19:10 +0100

    using the mvc model, for bundles at least

---
+++ Harmonizer.Front/App_Start/BundleConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Harmonizer.Front
+{
+	public class BundleConfig
+	{
+		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+
+			bundles.Add(new LessBundle("~/less").Include(
+				"~/css/_utils.less", 
+				"~/css/atomics.less",
+				"~/css/chord.less", 
+				"~/css/commands.less", 
+				"~/css/home.less"
+				));
+
+
+			bundles.Add(new ScriptBundle("~/bundles/libs").Include("~/js/vendor/angular.js", "~/js/vendor/howler.js"));
+			bundles.Add(new ScriptBundle("~/bundles/app").Include(
+				"~/js/app.js",
+				"~/js/config.js",
+				"~/js/controllers/mainController.js",
+				"~/js/directives/chordBlockDirective.js",
+				"~/js/directives/wheelSelect.js",
+				"~/js/factories/chordFactory.js",
+				"~/js/factories/soundFactory.js"
+				));
+
+			// Set EnableOptimizations to false for debugging. For more information,
+			// visit http://go.microsoft.com/fwlink/?LinkId=301862
+			BundleTable.EnableOptimizations = true;
+		}
+	}
+}
