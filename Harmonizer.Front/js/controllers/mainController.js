5306522861b47e85895cd530d137c693af4de1d3
Simon B <simon.budin@gmail.com>
2014-11-14 14:08:30 +0100

    Update mainController.js

--- Harmonizer.Front/js/controllers/mainController.js
+++ Harmonizer.Front/js/controllers/mainController.js
@@ -1,8 +1,11 @@
+﻿// TODO : use a module getter syntax instead of global app variable https://github.com/johnpapa/angularjs-styleguide#getters
+﻿// TODO : use IIFE https://github.com/johnpapa/angularjs-styleguide#iife
 ﻿app.controller('mainController', ['$scope', '$log' , 'soundFactory', 'chordFactory', 'configFactory',
 function ($scope, $log, soundFactory, chordFactory, configFactory)
 	// TODO : sortir les values de config dans une facto
 	// TODO : modification d'un chord après avoir cliqué dessus
 	// TODO : gestion du chord courant dans la lecture (style)
+	// TODO : use named functions instead of anonymous functions https://github.com/johnpapa/angularjs-styleguide#named-vs-anonymous-functions
 	{
 		$scope.notes = configFactory.notesConfig;
 		$scope.chordTypes = configFactory.chordTypesConfig;
