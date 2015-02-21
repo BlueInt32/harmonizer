64832fd495f1c80f6d4b0bd8410b8dbf3c264272
BlueInt32 <simon.budin@gmail.com>
2015-02-21 12:42:35 +0100

    added lodash factory

---
+++ Harmonizer.Front/js/utils/lodashFactory.js
@@ -0,0 +1,11 @@
+﻿(function(){
+	'use strict';
+	angular.module('app').factory('_', [
+		'$window',
+		function($window){
+			// place lodash include before angular
+			return $window._;
+		}
+	]);
+
+})();
