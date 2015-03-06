c58ec7346c7d5fd12d0712dd6402052e8b4c799b
BlueInt32 <simon.budin@gmail.com>
2015-03-06 08:44:31 +0100

    loader

--- Harmonizer.Front/js/app.js
+++ Harmonizer.Front/js/app.js
@@ -7,6 +7,22 @@
 			$logProvider.debugEnabled(true);
 		}
 	]);
+	angular.module('app').run(['$rootScope', function ($rootScope) {
+
+		$rootScope.$on('$routeChangeStart', function () {
+			$rootScope.stateIsLoading = true;
+		});
+
+
+		$rootScope.$on('$routeChangeSuccess', function () {
+			$rootScope.stateIsLoading = false;
+		});
+		$rootScope.$on('$routeChangeError', function (){
+			$rootScope.stateIsLoading = true;
+			$rootScope.apiError = true;
+		});
+
+	}]);
 
 	angular.module('app').config(['$routeProvider',
 		function ($routeProvider) {
@@ -14,28 +30,34 @@
 				.when('/',
 				{
 					controller: 'homeController',
-					controllerAs:'home',
+					controllerAs: 'home',
 					templateUrl: 'js/homePage/_home.tpl.html',
 					resolve: {
-						resolvedStaticData: ['staticDataService', 'soundFactory', 
-							function (staticDataService, soundFactory){
-							return staticDataService.getStaticData().then(function(staticData){
-								return soundFactory.inititalize(staticData);
-							});
-						}]
+						delay: function ($q, $timeout) {
+							console.log("resolve started");
+							var delay = $q.defer();
+							$timeout(delay.resolve, 1500);
+							return delay.promise;
+						},
+						resolvedStaticData: ['staticDataService', 'soundFactory',
+							function (staticDataService, soundFactory) {
+								return staticDataService.getStaticData().then(function (staticData) {
+									return soundFactory.inititalize(staticData);
+								});
+							}]
 					}
 				})
 				.when('/load/:seqId', {
 					controller: 'homeController',
-					controllerAs:'home',
+					controllerAs: 'home',
 					templateUrl: 'js/homePage/_home.tpl.html',
 					resolve: {
-						resolvedStaticData: ['staticDataService', 'soundFactory', 
-							function (staticDataService, soundFactory){
-							return staticDataService.getStaticData().then(function(staticData){
-								return soundFactory.inititalize(staticData);
-							});
-						}]
+						resolvedStaticData: ['staticDataService', 'soundFactory',
+							function (staticDataService, soundFactory) {
+								return staticDataService.getStaticData().then(function (staticData) {
+									return soundFactory.inititalize(staticData);
+								});
+							}]
 					}
 				});
 		}
