dc9bd474267957fd3ef31a1c1a4626b3854e9700
BlueInt32 <simon.budin@gmail.com>
2015-03-26 00:48:00 +0100

    burger directive

--- Harmonizer.Front/js/app/burgerMenu/burgerDirective.js
+++ Harmonizer.Front/js/app/burgerMenu/burgerDirective.js
@@ -1,42 +1,25 @@
 ﻿(function () {
 	'use strict';
 	angular.module('app')
-		.directive("burgerMenu", [
-			'$log',
-			function ($log) {
-				return {
-					restrict: 'A',
-					replace: true,
-					transclude: true,
-					scope: {},
-					templateUrl: 'js/app/burgerMenu/_burger.tpl.html',
-					controller: [
-						'$scope', '$log', '$rootScope', function ($scope, $log, $rootScope) {
-
-							$rootScope.$on('myCustomEvent', function (event, data){
-								$scope.openBurger();
-							});
-							$scope.isOpen = false;
-							$scope.openBurger = function () {
-								$scope.isOpen = true;
-							};
-							$scope.closeBurger = function () {
-								$scope.isOpen = false;
-							};
-						}
-					]
-				};
-			}
-		])
-	.directive("burgerMenuOpener", ['$rootScope', '$log', function ($rootScope, $log) {
-		return {
-			controller: ['$scope', 
-				function($scope){
-					$scope.broadcastOpenBurger = function(){
-						$rootScope.$broadcast('myCustomEvent', {});
+		.directive("burgerMenu", [function () {
+			return {
+				scope: false,
+				controller: function () {
+					var self = this;
+					this.openBurger = function () {
+						self.isOpen = true;
 					};
-				}
-			]
-	};
-	}]);
+					this.closeBurger = function () {
+						self.isOpen = false;
+					};
+					this.isOpen = false;
+				},
+				controllerAs: 'burgerCtrl',
+				restrict: 'E',
+				replace: true,
+				transclude: true,
+				templateUrl: 'js/app/burgerMenu/_burger.tpl.html'
+			}
+		}
+		]);
 })();
---
+++ Harmonizer.Front/js/app/burgerMenu/burgerService.js
@@ -0,0 +1,18 @@
+﻿(function () {
+	'use strict';
+	angular.module('app').service("burgerService",
+	['$scope', '$log',
+		function ($scope, $log){
+			var self = this;
+			this.isOpen = false;
+			this.openBurger = function () {
+				$log.debug('open');
+				$scope.isOpen = true;
+			};
+			this.closeBurger = function () {
+				$log.debug('close');
+				self.isOpen = false;
+			};
+		}
+	]);
+})();
