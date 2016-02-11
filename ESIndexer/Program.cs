19a1024375b67b0dd0ebf8c2aa95935b913f5c42
Simon Budin <simon.budin@gmail.com>
2016-02-11 09:06:53 +0100

    Setting up elastic search

---
+++ ESIndexer/Program.cs
@@ -0,0 +1,26 @@
+﻿using Harmonizer.Domain.Entities;
+using Harmonizer.Infrastructure.ElasticSearch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESIndexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SequenceES indexer = new SequenceES();
+            indexer.CreateIndex();
+
+            indexer.IndexSequence(new Sequence
+            {
+                Id = 1,
+                Name = "salut",
+                Description = "hello"
+            });
+        }
+    }
+}
---
+++ Harmonizer.Infrastructure.ElasticSearch/SequenceES.cs
@@ -0,0 +1,46 @@
+﻿using Elasticsearch.Net;
+using Harmonizer.Domain.Entities;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harmonizer.Infrastructure.ElasticSearch
+{
+    public class SequenceES
+    {
+        ElasticClient _client = null;
+        public SequenceES()
+        {
+            var node = new Uri("http://localhost:9200");
+
+            var settings = new ConnectionSettings(
+                node,
+                defaultIndex: "my-application"
+            );
+
+            _client = new ElasticClient(settings);
+        }
+
+        public void IndexSequence(Sequence sequence)
+        {
+            _client.Index(  sequence);
+        }
+
+        public void CreateIndex()
+        {
+            var settings = new IndexSettings();
+            settings.NumberOfReplicas = 1;
+            settings.NumberOfShards = 5;
+            settings.Settings.Add("merge.policy.merge_factor", "10");
+            settings.Settings.Add("search.slowlog.threshold.fetch.warn", "1s");
+
+            _client.CreateIndex(c => c
+                .Index("harmonizer")
+                .InitializeUsing(settings)
+            );
+        }
+    }
+}
